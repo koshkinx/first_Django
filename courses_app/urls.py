4cdb781661f479a5d78a794e4037d36a3cfb42c7
Dmytro Senchuk <koshkinx9@gmail.com>
2024-05-10 19:20:02 +0300

    Удалены все файлы из ветки master

--- courses_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.courses_page, name='courses_page'),
-]
--- courses_app/views.py
+++
@@ -1,6 +0,0 @@
-
-from django.shortcuts import render
-
-
-def courses_page(request):
-    return render(request, 'courses_app/courses_page.html')
