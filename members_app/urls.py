b7878286f650cab9d254fa051521acce12c04a82
Dmytro Senchuk <koshkinx9@gmail.com>
2024-05-12 18:51:43 +0300

    34

--- members_app/urls.py
+++ members_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('input/', views.input_page, name='input_page'),
     path('save/', views.save_input, name='save_input'),
     path('display/', views.display_page, name='display_input'),
+    path('session/', views.session_page, name='session_page'),
 ]
